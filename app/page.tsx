0cbe2b4caafaa694435250913dc29710fcafda20
Umar <m.umarbinejaz@gmail.com>
2023-08-31 18:16:29 +0500

    a todo list app for next practice

---
+++ app/page.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import prisma from "./db";
+import TodoItem from "@/components/TodoItem";
+
+function getToDos() {
+  return prisma.toDo.findMany();
+}
+
+export default async function Home() {
+  const todos = await getToDos();
+  return (
+    <>
+      <header className="flex justify-between mb-4 items-center">
+        <h1 className="text-2xl">Todos:</h1>
+        <Link
+          className="border border-slate-300 text-slate-300 
+        hover:bg-slate-700 focus-within:bg-slate-700 outline-none
+        px-2 py-1 rounded"
+          href="/new"
+        >
+          New
+        </Link>
+      </header>
+      <ul className="pl-4">
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} {...todo} />
+        ))}
+      </ul>
+    </>
+  );
+}
